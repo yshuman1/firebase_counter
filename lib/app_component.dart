78a8a1524ea6f78473c3406fe91812083e7104a1
Yasin Shuman <yshuman@loyalloops.com>
2017-06-14 00:43:55 -0700

    completed simple counter built with angulardart and firebase

---
+++ lib/app_component.dart
@@ -0,0 +1,55 @@
+// Copyright (c) 2017, yasin. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/angular2.dart';
+import 'package:angular_components/angular_components.dart';
+import 'package:firebase/firebase.dart';
+import 'dart:async';
+import 'src/todo_list/todo_list_component.dart';
+
+// AngularDart info: https://webdev.dartlang.org/angular
+// Components info: https://webdev.dartlang.org/components
+
+@Component(
+  selector: 'my-app',
+  styleUrls: const ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: const [materialDirectives, TodoListComponent],
+  providers: const [materialProviders],
+)
+class AppComponent implements OnInit {
+  int count = 0;
+  DatabaseReference ref;
+
+  @override
+  ngOnInit() {
+    initializeApp(
+        apiKey: "AIzaSyAH7S_gsce9RtNI8w0z7doiP3ugVJM8ZbI",
+        authDomain: "angulardart-firebase-io-2017.firebaseapp.com",
+        databaseURL: "https://angulardart-firebase-io-2017.firebaseio.com",
+        storageBucket: "angulardart-firebase-io-2017.appspot.com");
+
+    ref = database().ref('counter');
+    ref.onValue.listen((e) {
+      count = e.snapshot.val();
+    });
+  }
+  dislike() async {
+    count = await updateDatabase((c) => c - 1);
+  }
+
+  like() async {
+    count = await updateDatabase((c) => c + 1);
+  }
+
+  Future<int> updateDatabase(Function update) async {
+    var transaction = await ref.transaction((current) {
+      if (current != null) {
+        current = update(current);
+      }
+      return current;
+    });
+
+    return transaction.snapshot.val();
+  }
+}
